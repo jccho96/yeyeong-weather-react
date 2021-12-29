8512ac26c114b42a55f36dcddb3731439320b6e0
jccho96 <joanne.yy.cho@gmail.com>
2021-12-28 19:56:38 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Weather App
+        <footer>
+          {" "}
+          This project is created by yeyeong and is{" "}
+          <a
+            href="https://github.com/jccho96/yeyeong-weather-react.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
